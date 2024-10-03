7c2c573e5626ff604c80f85b53353384c7138708
radwa285 <radwa833@gmail.com>
2024-10-03 20:42:55 +0300

    Update package.json with correct design

--- src/components/Cardgrid.js
+++ src/components/Cardgrid.js
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ProductCard from './ProductCard';
+
 function ContainerExample() {
   const productData = [
     {
@@ -10,8 +11,8 @@ function ContainerExample() {
       image: 'https://m.media-amazon.com/images/I/61Pf0pZjl2L._AC_SY675_.jpg',
     },
     {
-      title: ' DREAM PAIRS ',
-      text: 'Ankle strap with adjustable buckle closure ',
+      title: 'DREAM PAIRS',
+      text: 'Ankle strap with adjustable buckle closure',
       image: 'https://m.media-amazon.com/images/I/718sKoZbhqL._AC_SY675_.jpg',
     },
     {
@@ -19,17 +20,17 @@ function ContainerExample() {
       text: 'Soft synthetic lining and lightly padded',
       image: 'https://m.media-amazon.com/images/I/71t4vsHpSOL._AC_SY675_.jpg',
     },
-    
   ];
+
   const productsData = [
     {
       title: 'The Drop Women',
-      text: 'Rectangular heel with a two strap detail ',
+      text: 'Rectangular heel with a two strap detail',
       image: 'https://m.media-amazon.com/images/I/61mFfFa0XJL._AC_SY675_.jpg',
     },
     {
-      title: ' Modatope Womens Sandals',
-      text: 'High Heel Strappy Sandals ',
+      title: 'Modatope Womens Sandals',
+      text: 'High Heel Strappy Sandals',
       image: 'https://m.media-amazon.com/images/I/61H3vJydzbL._AC_SY675_.jpg',
     },
     {
@@ -37,13 +38,13 @@ function ContainerExample() {
       text: 'Comfortable Insole Provides Support',
       image: 'https://m.media-amazon.com/images/I/71i0ICVJ7nL._AC_SY675_.jpg',
     },
-    
   ];
+
   return (
     <Container>
-      <Row >
+      <Row>
         {productData.map((product, index) => (
-          <Col key={index}>
+          <Col key={index} xs={12} sm={6} md={4} lg={3} className="mb-4">
             <ProductCard 
               title={product.title} 
               text={product.text} 
@@ -52,13 +53,13 @@ function ContainerExample() {
           </Col>
         ))}
       </Row>
-      <Row style={{marginTop:"20px"}}>
-        {productsData.map((Products, index) => (
-          <Col key={index}>
+      <Row style={{ marginTop: "20px" }}>
+        {productsData.map((product, index) => (
+          <Col key={index} xs={12} sm={6} md={4} lg={3} className="mb-4">
             <ProductCard 
-              title={Products.title} 
-              text={Products.text} 
-              image={Products.image} 
+              title={product.title} 
+              text={product.text} 
+              image={product.image} 
             />
           </Col>
         ))}
@@ -66,4 +67,5 @@ function ContainerExample() {
     </Container>
   );
 }
+
 export default ContainerExample;
