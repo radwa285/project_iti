48029e9ca153a4473f41df9689eabb86d5dbe3ca
radwa285 <radwa833@gmail.com>
2024-10-03 20:59:03 +0300

    Update package.json with correct design

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,14 +2,14 @@ import Card from 'react-bootstrap/Card';
 
 function ProductCard({ title, text, image }) {
   return (
-    <Card style={{ width: '22rem', margin: '1rem', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', borderRadius: '8px' }}>
+    <Card style={{ width: '22rem', margin: '1rem', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', borderRadius: '8px',marginLeft:"50px" }}>
       <Card.Img
         variant="top"
         src={image ? image : 'https://via.placeholder.com/150'} 
         alt={title}
         style={{ height: '400px', objectFit: 'cover', borderRadius: '8px 8px 0 0' }}
       />
-      <Card.Body>
+      <Card.Body >
         <Card.Title style={{ color: 'red', fontSize: '1.25rem', textAlign: 'center' }}>{title}</Card.Title>
         <Card.Text style={{ fontWeight: 'bold', fontSize: '1rem', textAlign: 'center', color: '#555' }}>
           {text}
